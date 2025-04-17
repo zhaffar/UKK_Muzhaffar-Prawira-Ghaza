8f2eefbeadea21797a43f6b7cf372300509de39a
Muzhaffar Prawira Ghaza <143237113+zhaffar@users.noreply.github.com>
2025-04-17 08:47:12 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let display = document.getElementById('display');
+
+function append(value) {
+  if (display.innerText === "0") {
+    display.innerText = value;
+  } else {
+    display.innerText += value;
+  }
+}
+
+function clearDisplay() {
+  display.innerText = "0";
+}
+
+function deleteLast() {
+  if (display.innerText.length > 1) {
+    display.innerText = display.innerText.slice(0, -1);
+  } else {
+    display.innerText = "0";
+  }
+}
+
+function calculate() {
+  try {
+    display.innerText = eval(display.innerText);
+  } catch (error) {
+    display.innerText = "Error";
+  }
+}
